8a89766c150b1502f93206eb3e065be4de5bb782
Dung Nguyen <dung34nguyen@gmail.com>
2020-06-04 16:10:51 -0500

    add constructor

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,7 +4,12 @@ import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults'
 import { Playlist } from '../Playlist/Playlist.js'
 class App extends React.Component {
-
+  constructor(props){
+    super(props);
+    this.state={
+      SearchResults:[{name: 'name1'},{artist: 'artist1'}, {album: 'album1' }, {id: 'id1' }] 
+    }
+  }
   render() {
     return (
       <div>
@@ -12,7 +17,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults  />
             <Playlist />
           </div>
         </div>
