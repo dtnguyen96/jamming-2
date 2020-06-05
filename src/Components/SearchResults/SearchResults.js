28932f34d7d89b7d94bd3698f3cc29cb79a4fb0c
Dung Nguyen <dung34nguyen@gmail.com>
2020-06-05 18:47:56 -0500

    debug

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './SearchResults.css'
-import {TrackList} from './Tracklist/Tracklist';
+import {Tracklist} from './Tracklist/Tracklist';
 export class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                <TrackList tracks={this.props.searchResults}/>
+                <Tracklist tracks={this.props.searchResults}/>
             </div>
         )
     }
