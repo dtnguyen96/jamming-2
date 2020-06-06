6fac3d3a0e324eeb83ecc2bda19d11f81155beb5
Dung Nguyen <dung34nguyen@gmail.com>
2020-06-06 18:06:37 -0500

    render Tracklist in Playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./Playlist.css"
-//import {Tracklist} from './Tracklist/Tracklist';
+import {Tracklist} from '../Tracklist/Tracklist';
 export class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
                 <input defaultValue={'New Playlist'} />
-             {/*   <Tracklist /> */}
+                <Tracklist tracks={this.props.playlistTracks} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
