b67ebcf40b509940654f3e557c6b6c898b8d1439
Dung Nguyen <dung34nguyen@gmail.com>
2020-06-05 23:42:42 -0500

    add addTrack and pass it as props to SearchResults

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,6 +12,17 @@ class App extends React.Component {
       playlistTracks: [{ name: 'name2', artist: 'artist2', album: 'album2', id: 'id2' }]
 
     }
+    this.addTrack=this.addTrack.bind(this);
+  }
+  //function add individuals song to the playlist
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack=>savedTrack.id===track.id)) {
+      return;
+    }
+    else {
+      this.playlistTracks.push(track)
+    }
+
   }
   render() {
     return (
@@ -20,7 +31,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
