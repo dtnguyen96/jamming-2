59ea3d0fa370cb9a066782cfffafac47fc4d7942
Dung Nguyen <dung34nguyen@gmail.com>
2020-06-06 21:06:11 -0500

    add method to change playlist name

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack=this.removeTrack.bind(this);
+    this.updatePlaylistName=this.updatePlaylistName.bind(this);
   }
   //function to add individuals track to the playlist
   addTrack(track) {
@@ -35,6 +36,10 @@ class App extends React.Component {
     tracks=tracks.filter(track => track.id != trackPassin.id)
     this.setState({ playlistTracks: tracks });
   }
+  //function to update Playlist name
+  updatePlaylistName(name){
+    this.setState({playlistName: name});
+  }
   render() {
     return (
       <div>
@@ -43,7 +48,8 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} 
+            onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,10 +2,17 @@ import React from 'react';
 import "./Playlist.css"
 import {Tracklist} from '../Tracklist/Tracklist';
 export class Playlist extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleNameChange=thhis.handleNameChange.bind(this);
+    }
+    handleNameChange(event){
+        this.props.onNameChange(event.target.value);
+    }
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={'New Playlist'} />
+                <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
                 <Tracklist tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
