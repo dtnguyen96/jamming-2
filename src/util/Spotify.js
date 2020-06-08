98e7968b0dc76bafddc75800058969bca4d7a07a
Dung Nguyen <dung34nguyen@gmail.com>
2020-06-07 22:48:58 -0500

    finish method getAccessToken

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,29 @@
-const userAccessToken=''
+
+let userAccessToken;
+//module to communitcate with Spotify API
 Spotify={
-    //method 
+    //method to get get user access token
+    getAccessToken(){
+        if  (userAccessToken){
+            return userAccessToken;
+        }
+        //ELSE: extract the access token and expire time from the URL obtained
+        //window.locattion.href returns current site URL
+        const userAccessTokenMatch=window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch=window.location.href.match(/expires_in=([^&]*)/);
+        //If access token and expiration time is in the url then set those variables up
+        if (userAccessTokenMatch && expiresInMatch){
+            userAccessToken=userAccessTokenMatch[1];
+            const expiresIn=Number(expiresInMatch[1]);
+        }
+        
+        /*Clear the parameters from the URL, 
+        so the app doesn’t try grabbing the access token after it has expired */
+        window.setTimeout(() => userAccessToken = '', expiresInMatch* 1000);
+        window.history.pushState('Access Token', null, '/');
+        return userAccessToken
+    }
+
+
 }
-export default spotify
+export default Spotify;
